87d1df97b84c53aef5154bd876412a29ebcd12f5
Ray Cassani <raymundo.cassani@gmail.com>
2017-10-22 22:11:39 -0400

    Add flag for optional Bias terms

--- mlp.py
+++ mlp.py
@@ -28,7 +28,7 @@ class Mlp():
     fully-connected Multi-Layer Perceptron (MLP)
     '''
 
-    def __init__(self, size_layers, act_funct='sigmoid', reg_lambda=0):
+    def __init__(self, size_layers, act_funct='sigmoid', reg_lambda=0, bias_flag=True):
         '''
         Constructor method. Defines the characteristics of the MLP
 
@@ -39,34 +39,36 @@ class Mlp():
                 default = 'sigmoid'
             reg_lambda: Value of the regularization parameter Lambda
                 default = 0, i.e. no regularization
+            bias: Indicates is the bias element is added for each layer, but the output
         '''
         self.size_layers = size_layers
         self.n_layers    = len(size_layers)
         self.act_f       = act_funct
         self.lambda_r    = reg_lambda
-
+        self.bias_flag   = bias_flag
+ 
         # Ramdomly initialize theta (MLP weights)
         self.initialize_theta_weights()
 
-    def train(self, X, y, iterations=400, reset=False):
+    def train(self, X, Y, iterations=400, reset=False):
         '''
         Given X (feature matrix) and y (class vector)
         Updates the Theta Weights by running Backpropagation N tines
         Arguments:
             X          : Feature matrix [n_examples, n_features]
-            y          : Class vector [n_examples]
+            Y          : Sparse class matrix [n_examples, classes]
             iterations : Number of times Backpropagation is performed
                 default = 400
             reset      : If set, initialize Theta Weights before training
                 default = False
         '''
-        n_examples = len(y)
-        self.labels = np.unique(y)
-        Y = np.zeros((n_examples, len(self.labels)))
-        for ix_label in range(len(self.labels)):
-            # Find examples with with a Label = lables(ix_label)
-            ix_tmp = np.where(y == self.labels[ix_label])[0]
-            Y[ix_tmp, ix_label] = 1
+        n_examples = Y.shape[0]
+#        self.labels = np.unique(y)
+#        Y = np.zeros((n_examples, len(self.labels)))
+#        for ix_label in range(len(self.labels)):
+#            # Find examples with with a Label = lables(ix_label)
+#           ix_tmp = np.where(y == self.labels[ix_label])[0]
+#            Y[ix_tmp, ix_label] = 1
 
         if reset:
             self.initialize_theta_weights()
@@ -87,9 +89,7 @@ class Mlp():
         '''
         A , Z = self.feedforward(X)
         Y_hat = A[-1]
-        y_tmp = np.argmax(Y_hat, axis=1)
-        y_hat = self.labels[y_tmp]
-        return y_hat
+        return Y_hat
 
     def initialize_theta_weights(self):
         '''
@@ -107,13 +107,19 @@ class Mlp():
                 # Xavier Glorot and Youshua Bengio, 2010
                 epsilon = 4.0 * np.sqrt(6) / np.sqrt(size_layer + size_next_layer)
                 # Weigts from a uniform distribution [-epsilon, epsion]
-                theta_tmp = epsilon * ( (np.random.rand(size_next_layer, size_layer + 1) * 2.0 ) - 1)
+                if self.bias_flag:  
+                    theta_tmp = epsilon * ( (np.random.rand(size_next_layer, size_layer + 1) * 2.0 ) - 1)
+                else:
+                    theta_tmp = epsilon * ( (np.random.rand(size_next_layer, size_layer) * 2.0 ) - 1)            
             elif self.act_f == 'relu':
                 # Method presented in "Delving Deep into Rectifiers: Surpassing Human-Level Performance on ImageNet Classfication"
                 # He et Al. 2015
                 epsilon = np.sqrt(2.0 / (size_layer * size_next_layer) )
                 # Weigts from Normal distribution mean = 0, std = epsion
-                theta_tmp = epsilon * (np.random.randn(size_next_layer, size_layer + 1 ));
+                if self.bias_flag:
+                    theta_tmp = epsilon * (np.random.randn(size_next_layer, size_layer + 1 ))
+                else:
+                    theta_tmp = epsilon * (np.random.randn(size_next_layer, size_layer))                    
             self.theta_weights.append(theta_tmp)
         return self.theta_weights
 
@@ -136,8 +142,9 @@ class Mlp():
         # For the second last layer to the second one
         for ix_layer in np.arange(self.n_layers - 1 - 1 , 0 , -1):
             theta_tmp = self.theta_weights[ix_layer]
-            # Removing weights for bias
-            theta_tmp = np.delete(theta_tmp, np.s_[0], 1)
+            if self.bias_flag:
+                # Removing weights for bias
+                theta_tmp = np.delete(theta_tmp, np.s_[0], 1)
             deltas[ix_layer] = (np.matmul(theta_tmp.transpose(), deltas[ix_layer + 1].transpose() ) ).transpose() * g_dz(Z[ix_layer])
 
         # Compute gradients
@@ -145,8 +152,12 @@ class Mlp():
         for ix_layer in range(self.n_layers - 1):
             grads_tmp = np.matmul(deltas[ix_layer + 1].transpose() , A[ix_layer])
             grads_tmp = grads_tmp / n_examples
-            # Regularize weights, except for bias weigths
-            grads_tmp[:, 1:] = grads_tmp[:, 1:] + (self.lambda_r / n_examples) * self.theta_weights[ix_layer][:,1:]
+            if self.bias_flag:
+                # Regularize weights, except for bias weigths
+                grads_tmp[:, 1:] = grads_tmp[:, 1:] + (self.lambda_r / n_examples) * self.theta_weights[ix_layer][:,1:]
+            else:
+                # Regularize ALL weights
+                grads_tmp = grads_tmp + (self.lambda_r / n_examples) * self.theta_weights[ix_layer]       
             gradients[ix_layer] = grads_tmp;
         return gradients
 
@@ -165,8 +176,9 @@ class Mlp():
 
         for ix_layer in range(self.n_layers - 1):
             n_examples = input_layer.shape[0]
-            # Add bias element to every example in input_layer
-            input_layer = np.concatenate((np.ones([n_examples ,1]) ,input_layer), axis=1)
+            if self.bias_flag:
+                # Add bias element to every example in input_layer
+                input_layer = np.concatenate((np.ones([n_examples ,1]) ,input_layer), axis=1)
             A[ix_layer] = input_layer
             # Multiplying input_layer by theta_weights for this layer
             Z[ix_layer + 1] = np.matmul(input_layer,  self.theta_weights[ix_layer].transpose() )
@@ -197,10 +209,14 @@ class Mlp():
         size_next_layers = self.size_layers.copy()
         size_next_layers.pop(0)
         rolled_list = []
+        if self.bias_flag:
+            extra_item = 1
+        else:
+            extra_item = 0
         for size_layer, size_next_layer in zip(self.size_layers, size_next_layers):
-            n_weights = (size_next_layer * (size_layer + 1))
+            n_weights = (size_next_layer * (size_layer + extra_item))
             data_tmp = unrolled_data[0 : n_weights]
-            data_tmp = data_tmp.reshape(size_next_layer, (size_layer + 1), order = 'F')
+            data_tmp = data_tmp.reshape(size_next_layer, (size_layer + extra_item), order = 'F')
             rolled_list.append(data_tmp)
             unrolled_data = np.delete(unrolled_data, np.s_[0:n_weights])
         return rolled_list
